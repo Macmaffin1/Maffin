2a15ff85e23eee1888c3afb647f9aa62dd0bd5b7
Maffin <tvorojok70@gmail.com>
2019-04-18 22:05:27 +0300

    Hw01_easy

---
+++ решенные задачи/Уроки/hw01_easy.py/Задача-2.py
@@ -0,0 +1,5 @@
+__author__ = 'Мельник Виктория'
+variable_1 = input('Введите значение 1 ')
+variable_2 = input('Введите значение 2 ')
+variable_1, variable_2 = variable_2, variable_1
+print(variable_1, variable_2)
